2d831fd4ce03a42575982f8e251538c19416e5d4
Vivkthkre <40302511+Vivkthkre@users.noreply.github.com>
2018-06-16 16:56:20 +0000

    Delete checkakeyindict.py

--- checkakeyindict.py
+++
@@ -1,6 +0,0 @@
-mydict={'name':'Ken','course':'Python','roll':56,'Branch':'EC',1:2,2:4,3:9}
-key=input("Enter key to check:")
-if key in mydict.keys():
-    print("Key exists")
-else:
-    print("Key does not exists")
