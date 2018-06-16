9f5bd1ac2d70cc6e2229f6acaca65ec4a639167e
Vivkthkre <40302511+Vivkthkre@users.noreply.github.com>
2018-06-16 16:43:26 +0530

    Delete merge2pydicts.py

--- merge2pydicts.py
+++
@@ -1,6 +0,0 @@
-dict1={1:1,2:2,3:3,4:4,5:5}
-dict2={'name':'San','cousre':'JAVA','Branch':'CSE','Roll':76}
-print('D1=',dict1)
-print('D2=',dict2)
-dict1.update(dict2)
-print("Dictionary after merging:\n",dict1)
