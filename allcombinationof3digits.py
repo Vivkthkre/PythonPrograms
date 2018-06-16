dcb2acc33b3b1700e7f7bdc429ec736fa6888786
Vivkthkre <40302511+Vivkthkre@users.noreply.github.com>
2018-06-16 14:01:27 +0530

    all combination of 3 digits
    take 3 distinct digits and print all combination of the 3digits

---
+++ allcombinationof3digits.py
@@ -0,0 +1,8 @@
+n1=int(input("Enter number1:"))
+n2=int(input("Enter number2:"))
+n3=int(input("Enter number3:"))
+n=[n1,n2,n3]
+for i in n:
+    for j in n:
+        for k in n:
+            print(i,j,k)  
